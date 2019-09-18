3373e3b0003d1731dd3368d1fd3b50d2684ca0b6
Student <example@example.com>
2019-09-18 16:03:25 -0700

    Function Components

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// element and render are inside of fucntion tick()
-
-const tick = () => {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+function Welcome (props) {
+  console.log(props)
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Welcome />,
+  document.getElementById('root')
+);
+
   // If you want your app to work offline and load faster, you can change
   // unregister() to register() below. Note this comes with some pitfalls.
   // Learn more about service workers: https://bit.ly/CRA-PWA
