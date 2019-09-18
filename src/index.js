f08af2207aab863e8e2330709f2bde49eddea488
Student <example@example.com>
2019-09-18 15:00:19 -0700

    JSX element with children

--- src/index.js
+++ src/index.js
@@ -17,14 +17,19 @@ const getGreeting = (user) => {
 }
 
 const user = { firstName: 'Harper', lastName:'Perez'};
-const element = <h1>{getGreeting(user)}</h1>
+const element = (
+  <div>
+    <img src={'https://twt-thumbs.washtimes.com/media/image/2016/04/01/twins_nationals_baseball_c0-103-4776-2887_s885x516.jpeg?dc43a1c189d05de06314ef8906abac5257f733cb'}></img>
+    <h1>{getGreeting(user)}</h1>
+  </div>)
+
 
 ReactDOM.render(
   element,
   // <App />,
   document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
