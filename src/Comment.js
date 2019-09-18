8c16d212e3854cd9545f25d05245174c0a7c0129
Student <example@example.com>
2019-09-18 16:46:26 -0700

    splitting components

---
+++ src/Comment.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+// This is bed practice to reuse components. Don’t be afraid to split components into smaller components.
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <img className="Avatar"
+//           src={props.author.avatarUrl}
+//           alt={props.author.name}
+//         />
+//         <div className="UserInfo-name">
+//           {props.author.name}
+//         </div>
+//       </div>
+//       <div className="Comment-text">
+//         {props.text}
+//       </div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+// The Avatar doesn’t need to know that it is being rendered inside a Comment.
+// This is why we have given its prop a more generic name: user rather than author.
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}/>
+    );
+}
+
+// Avatar --> UserInfo --> Comment rather than everything in comment!
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+export default Comment;
