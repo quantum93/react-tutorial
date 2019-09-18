ce40eef6c58ab5ccdd88d75f9a13435cf137330e
Student <example@example.com>
2019-09-18 14:38:11 -0700

    JSX with function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const name = 'Josh Perez';
-const element = <h1> Hello, {name}</h1>
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = { firstName: 'Harper', lastName:'Perez'};
+
+const element = (<h1> Hello, {formatName(user)}</h1>)
 
 ReactDOM.render(
   element,
