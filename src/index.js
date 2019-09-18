844e6890e42fa49267f47a8a27c4b7e74de1e23c
Student <example@example.com>
2019-09-18 16:09:19 -0700

    Class Components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Welcome (props) {
-  console.log(props)
-  return <h1>Hello, {props.name}</h1>;
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
 }
 
 ReactDOM.render(
