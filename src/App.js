1e8dd4d3b703a53a89d2005342c71fb864ea4a90
Student <example@example.com>
2019-09-18 16:27:21 -0700

    Composing components in one more splitting files.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
-
-// class Welcome extends React.Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}</h1>;
-//   }
-// }
-
-function Welcome (props) { return <h1>Hello, {props.name}</h1>; }
+import Welcome from './Welcome';
 
 function App() {
   return (
