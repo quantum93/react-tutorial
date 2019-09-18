83565ab58c8b8464454998865472458d2f8849b8
Student <example@example.com>
2019-09-18 14:42:02 -0700

    JSX element without () but not recommended

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 const formatName = (user) => {return user.firstName + ' ' + user.lastName;}
 const user = { firstName: 'Harper', lastName:'Perez'};
-const element = (<h1> Hello, {formatName(user)}</h1>)
+const element = <h1> Hello, {formatName(user)}</h1>
 
 ReactDOM.render(
   element,
