a184aec1afa19abfaad388621711f66ce2ea8dfa
Student <example@example.com>
2019-09-23 12:58:19 -0700

    It refactors event handle

--- src/Toggle.js
+++ src/Toggle.js
@@ -4,15 +4,24 @@ class Toggle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {isToggleOn: true};
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
+    // This binding is necessary to make `this` work in the callback. if you use fat arrow function, don't need this line
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
+ //In class 'const' is not necessary to define fat arrow function.
+  handleClick = () => {
+    this.setState(
+      state => ({isToggleOn: !state.isToggleOn})
+    );
   }
+
+  // handleClick() {
+  //   this.setState(
+  //     function state() {
+  //       return {isToggleOn: !state.isToggleOn}
+  //     }
+  //   );
+  // }
 
   render() {
     return (
