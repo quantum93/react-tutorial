1ca75842d523bdf8c4094fec02cb2461ee4ed36a
Student <example@example.com>
2019-09-24 13:28:16 -0700

    It shows how to do lifting state up

--- src/TemperatureInput.js
+++ src/TemperatureInput.js
@@ -5,15 +5,17 @@ const scaleNames = { c: 'Celsius', f: 'Fahrenheit' };
 class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {temperature: ''};
+    // this.state = {temperature: ''};
   }
 
   handleChange = (event) => {
-    this.setState({temperature: event.target.value});
+    // this.setState({temperature: event.target.value});
+    this.props.onTemperatureChange(event.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    // const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return(
       <fieldset>
