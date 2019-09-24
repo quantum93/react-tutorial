207f168779ba02528b1b08d8cafdc24a661e292a
Student <example@example.com>
2019-09-24 11:58:47 -0700

    It adds second input for temperature conversion

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,34 +1,22 @@
 import React from 'react';
-
+import TemperatureInput from './TemperatureInput';
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {temperature: ''};
-  }
-
-  handleChange = (event) => {
-    this.setState({temperature: event.target.value});
-  }
-
-  BoilingVerdict(props) {
-    if (props.celsius >= 100) {
-      return <p>The water would boil.</p>;
-    }
-    return <p>The water would not boil.</p>;
-  }
+  // BoilingVerdict(props) {
+  //   if (props.celsius >= 100) {
+  //     return <p>The water would boil.</p>;
+  //   }
+  //   return <p>The water would not boil.</p>;
+  // }
 
   render() {
-    const temperature = this.state.temperature;
     return(
-      <fieldset>
-        <legend>Enter temperature in Celsius</legend>
-        <input value={temperature} onChange={this.handleChange} />
-        <this.BoilingVerdict celsius={parseFloat(temperature)} />
-      </fieldset>
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
     );
   }
 }
-
 
 export default Calculator;
